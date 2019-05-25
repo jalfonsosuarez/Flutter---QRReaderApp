1d7cda4326acc737be90c1fcd04d171f153ccd51
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-25 23:38:42 +0200

    [149] FloatingAcctionButton y tema de la app.

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -16,8 +16,24 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text( 'QR Scanner' ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon( Icons.delete_forever ),
+            onPressed: () {},
+          )
+        ],
+      ),
       body: _callPage( pagina ),
       bottomNavigationBar: _crearBottomNavigationBar(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      floatingActionButton: FloatingActionButton(
+        child: Icon( Icons.filter_center_focus ),
+        onPressed: () {},
+        backgroundColor: Theme.of( context ).primaryColor,
+      ),
+
     );
   }
 
