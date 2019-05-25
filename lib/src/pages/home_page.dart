5db3c9c48b38c8b5787201958565f9c6095e2c39
Jose Alfonso Suarez Moreno <alfonso@Mac-de-Alfonso.local>
2019-05-25 20:48:19 +0200

    [148] BottomNavigator. Páginas Mapas y Direcciones

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,15 +1,58 @@
 import 'package:flutter/material.dart';
+import 'package:qrreaderapp/src/pages/direcciones_page.dart';
+import 'package:qrreaderapp/src/pages/mapas_page.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
 
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+
+  int pagina=0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text( 'Home page!'),
-      )
+      body: _callPage( pagina ),
+      bottomNavigationBar: _crearBottomNavigationBar(),
     );
   }
-  
+
+  Widget _crearBottomNavigationBar() {
+
+    return BottomNavigationBar(
+      currentIndex: pagina,
+      onTap: (index) {
+        setState(() {
+          pagina = index;
+        });
+      },
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon( Icons.map ),
+          title: Text( 'Mapas' ),
+        ),
+        BottomNavigationBarItem(
+          icon: Icon( Icons.brightness_5 ),
+          title: Text( 'Direcciones' ),
+        ),
+      ]
+    );
+
+  }
+
+  Widget _callPage( int pagina ) {
+
+    switch( pagina ){
+      case 0: return MapasPage();
+      case 1: return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+
+  }
 }
